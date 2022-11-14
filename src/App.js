32421497f91538a5f4f94282c701838ece99f4e7
Angelos <kounavisangelo@gmail.com>
2022-11-14 16:59:24 -0500

    starting city explorer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: ''
+    }
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    let swData = await axios.get('https://swap.dev/api/people/?page=1');
+    // this.setState({
+    //   city: e.target.city.value
+    // });
+    this.setState({
+      starWarsData: swData.data.result
+    });
+    console.log(swData.data.results);
+  }
+
+  HandleInputChange = (e) => {
+    this.setState({
+      city: e.target.value
+    });
+  }
+
+
+
+
+  render() {
+
+    this.state.starWarsData.map((char, idx) => {
+      return <li key={idx}>{char.name}</li>;
+    });
+    return (
+      <>
+        <h1>City Explorer</h1>
+        <from onSubmit={this.handleSubmit}>
+          <label>Search for a City
+            <input type="text" name="city" onChange={this.HandleInputChange} />
+          </label>
+          <button type="submit">Search for a City</button>
+        </from>
+        <label>
+          <input name="city type="text" onChange/>
+        </label>
+        <button type="submit">Get City Data</button>
+        <uL>
+          {swCharacters}
+        </uL>
+      </>
+    )
+  }
 }
 
-export default App;
+
+export default App
